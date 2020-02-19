ba841ee159731b09932f8071cd75a1ea349b21bb
teresalr <s013272@students.lmsd.org>
2020-02-19 14:16:02 -0500

    Initial Commit

---
+++ EcoSave/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EcoSave
+//
+//  Created by Teresa Lu-Romeo (student LM) on 2/19/20.
+//  Copyright © 2020 Teresa Lu-Romeo (student LM). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
