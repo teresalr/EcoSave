3aed00081be2904f334885abecdbd8a61e9f1cd8
dhavalsharmaLMHS <s015002@students.lmsd.org>
2020-02-27 12:26:42 -0500

    New controller

---
+++ EcoSave/PopUpViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PopUpViewController.swift
+//  EcoSave
+//
+//  Created by Dhaval Sharma (student LM) on 2/27/20.
+//  Copyright © 2020 Teresa Lu-Romeo (student LM). All rights reserved.
+//
+
+import UIKit
+
+class PopUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
