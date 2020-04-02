d466d57f66889562578bd0346ca1a20f2eefa622
teresalr <s013272@students.lmsd.org>
2020-04-02 10:39:46 -0400

    uh oh

--- EcoSave/AppDelegate.swift
+++ EcoSave/AppDelegate.swift
@@ -21,9 +21,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // MARK: UISceneSession Lifecycle
 
     @available(iOS 13.0, *)
-    @available(iOS 13.0, *)
-    @available(iOS 13.0, *)
-    @available(iOS 13.0, *)
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
---
+++ EcoSave/RandomViewController.swift
@@ -0,0 +1,35 @@
+//
+//  RandomViewController.swift
+//  EcoSave
+//
+//  Created by Teresa Lu-Romeo (student LM) on 4/2/20.
+//  Copyright © 2020 Teresa Lu-Romeo (student LM). All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class RandomViewController: UIViewController {
+
+    @IBAction func switchScreen(_ sender: UIButton) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
